50d800902550d54047c00531ad674ef6d81803dc
Boeckle <dboeckle@schelling.at>
2021-09-16 20:30:39 +0200

    feat: go add init from Prelude and with take

--- 1_First_Steps_in_Haskell/List.hs
+++ 1_First_Steps_in_Haskell/List.hs
@@ -7,8 +7,17 @@ listLast [] = error "Can't do last of an empty list!"
 
 listLast1 :: [a] -> a
 listLast [x] = x --base case is when there's just one element remaining
-listLast x =  x!!(length x -1)
+listLast x =  x!!(length x-1)
 
 listLast2 :: [a] -> a
 listLast [x] = x --base case is when there's just one element remaining
 listLast x =  head (reverse x)
+
+--Aufgabe init
+myinit :: [a] -> [a]
+myinit [x] = [] --base case is when there's just one element remaining
+myinit (x:xs) = x : myinit xs ---recursive call until there is no more successor
+
+myinit1 :: [a] -> [a]
+myinit1 [x] = [] --base case is when there's just one element remaining
+myinit1 (x) = take (length x-1) x
