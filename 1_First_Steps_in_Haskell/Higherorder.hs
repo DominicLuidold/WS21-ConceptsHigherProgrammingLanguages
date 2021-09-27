b05b5a7ff6dd1febc82deab1c613a71799a8c7e8
Dominik Boeckle <dbo2019@students.fhv.at>
2021-09-27 20:56:46 +0200

    feat: add TODO

--- 1_First_Steps_in_Haskell/Higherorder.hs
+++ 1_First_Steps_in_Haskell/Higherorder.hs
@@ -1,4 +1,8 @@
 import Data.Char (toLower)
+-- TODO
+-- Pipelining (1 Point)
+-- Using higher-order functions write a function pipeline which implements a transformation pipeline on lists of integers which multiplies each element by its index (starting with 1!), removes the odd values, and computes the sum. For example
+
 palindrome :: Eq a => [a] -> Bool
 palindrome xs = reverse xs == xs
 
