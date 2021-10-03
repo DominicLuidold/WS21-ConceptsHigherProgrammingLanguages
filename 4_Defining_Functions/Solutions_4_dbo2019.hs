74a631aaf8d570799dcfa142da65819600159aa3
Dominic Luidold <dominic.luidold@students.fhv.at>
2021-10-03 19:38:31 +0200

    Split Dominik's solutions into appropriate files

--- 4_Defining_Functions/Solutions_4_dbo2019.hs
+++ 4_Defining_Functions/Solutions_4_dbo2019.hs
@@ -64,16 +64,3 @@ pack (x:xs) = let (first,rest) = span (==x) xs -- https://hackage.haskell.org/pa
 
 encode :: Eq a => [a] -> [(Int, a)]
 encode = map (\x -> (length x,head x)) . pack
-
-
-merge :: Ord a => [a] -> [a] -> [a]
-merge x [] = x
-merge [] x = x
-merge l1@(x:xs) l2@(y:ys)
-  | x < y     = x:(merge xs l2)
-  | otherwise = y:(merge l1 ys)
-
-msort :: Ord a => [a] -> [a]
-msort [] = []
-msort [x] = [x]
-msort x =  merge (msort (fst (halve x))) (msort (snd (halve x)))
---
+++ 5_Recursive_Functions/Solutions_5_dbo2019.hs
@@ -0,0 +1,11 @@
+merge :: Ord a => [a] -> [a] -> [a]
+merge x [] = x
+merge [] x = x
+merge l1@(x:xs) l2@(y:ys)
+  | x < y     = x:(merge xs l2)
+  | otherwise = y:(merge l1 ys)
+
+msort :: Ord a => [a] -> [a]
+msort [] = []
+msort [x] = [x]
+msort x =  merge (msort (fst (halve x))) (msort (snd (halve x)))
