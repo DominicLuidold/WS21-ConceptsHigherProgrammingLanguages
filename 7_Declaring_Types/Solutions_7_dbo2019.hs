b479698d6c41d3c3fa6bfb584f8ff895dfc0da27
dpb97 <dominikpboeckle@gmail.com>
2021-10-20 20:33:34 +0200

    feat: add count Leafs  and Tree Depth

--- 7_Declaring_Types/Solutions_7_dbo2019.hs
+++ 7_Declaring_Types/Solutions_7_dbo2019.hs
@@ -9,7 +9,7 @@ nat2int :: Nat -> Int
 nat2int Zero     = 0
 nat2int (Succ n) = 1 + nat2int n
 
-data Tree a = Leaf a | Node a (Tree a) (Tree a)
+data Tree a = Leaf a | Node  (Tree a) a (Tree a) deriving Show
 
 add :: Nat -> Nat -> Nat
 add Zero     n = n
@@ -38,11 +38,11 @@ sub (Succ m) (Succ n) = sub m n
 
 checkcomplete :: Tree a -> Bool 
 checkcomplete (Leaf a) = True
-checkcomplete (Node x a b) = checkcomplete a && checkcomplete b && abs( checkheight a - checkheight  b) <=1
+checkcomplete (Node a x b) = checkcomplete a && checkcomplete b && abs( checkheight a - checkheight  b) <=1
 
 checkheight :: Tree a -> Int
 checkheight (Leaf _) = 0
-checkheight (Node a x y) = max(checkheight x) (checkheight y) + 1 --https://www.geeksforgeeks.org/write-a-c-program-to-find-the-maximum-depth-or-height-of-a-tree/
+checkheight (Node l _ r) = max(checkheight l) (checkheight r) + 1 --https://www.geeksforgeeks.org/write-a-c-program-to-find-the-maximum-depth-or-height-of-a-tree/
 
 
 sub' :: Nat -> Nat -> Nat
@@ -53,3 +53,13 @@ sub' (Succ m) (Succ n) = sub m n
 division :: Nat -> Nat -> Nat
 division _ Zero = Zero
 division x y = sub x y
+
+-- Binary Tree
+countLeafs :: Tree a -> Int
+countLeafs (Leaf _) = 1
+countLeafs (Node l _ r) = countLeafs l + countLeafs r
+
+treeDepth  :: Tree a -> Int -- Tiefe startet mit 0
+treeDepth  (Leaf _) = 0
+treeDepth  (Node l _ r) = max(treeDepth  l) (treeDepth  r) + 1 --https://www.geeksforgeeks.org/write-a-c-program-to-find-the-maximum-depth-or-height-of-a-tree/
+-- Node (Node (Leaf 1) 3 (Leaf 4)) 5 (Node (Leaf 6) 7 (Leaf 9))
