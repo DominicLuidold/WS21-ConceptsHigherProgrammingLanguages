5af08e6b0d4dbaa2a8abcf2d99c90166d8b85d08
dpb <dominikpboeckle@gmail.com>
2021-11-10 15:42:49 +0100

    feat: add

--- 9_Type_Classes/Solutions_9_dbo2019.hs
+++ 9_Type_Classes/Solutions_9_dbo2019.hs
@@ -4,3 +4,21 @@ data Tree a = Leaf a
 instance Functor Tree where
     fmap f (Leaf x)  = Leaf (f x)
     fmap f (Branch  l r) = Branch (fmap f l) (fmap f r)     
+
+newtype ZipList a = Z [a] deriving Show
+
+instance Functor ZipList where
+  -- fmap :: (a -> b) -> ZipList a -> ZipList b
+  fmap g (Z xs) = Z (map g xs)
+
+instance Applicative ZipList where
+  -- pure :: a -> ZipList a
+  pure x = Z [x]
+  
+  -- <*> :: ZipList (a -> b) -> ZipList a -> ZipList b
+  (Z gs) <*> (Z xs) = Z (gs xs)
+
+
+
+test:: [a -> b] -> [a] -> [b]
+test f x = f x
