1212942df65c7079843d8a9af7b39624944efda5
Dominic Luidold <dominic.luidold@students.fhv.at>
2022-01-06 17:54:28 +0100

    Add submission-ready solutions for chapter 8.
    Co-authored-by: dpb97 <dominikpboeckle@gmail.com>

---
+++ 8_Lazy_Evaluation/Solutions_8.hs
@@ -0,0 +1,7 @@
+-- 8 Lazy Evaluations
+
+-- 8.7.4 - Lazy Fibonacci
+-- Improved by https://stackoverflow.com/a/50062271
+lazyFibonacci :: [Integer]
+lazyFibonacci = lazyFibonacci' 0 1 where
+    lazyFibonacci' x y = x : lazyFibonacci' y (x + y)
