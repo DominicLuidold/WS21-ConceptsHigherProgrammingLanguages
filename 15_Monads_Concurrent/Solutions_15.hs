056f0050a1b8670f154d0a7917050b53f5891371
dpb97 <dominikpboeckle@gmail.com>
2022-01-08 12:52:44 +0100

    add IOREF Concurrent

--- 15_Monads_Concurrent/Solutions_15.hs
+++ 15_Monads_Concurrent/Solutions_15.hs
@@ -3,15 +3,20 @@ import Control.Concurrent.Async (wait, async)
 -- 15 Monads In Use: Parallel and Concurrent Programming
 
 -- 15.3.1 Primes IORef
-primesConcurrent :: (Integer,Integer) -> IO ()
-primesConcurrent (x, y) = do
-    ref <- newIORef 1
-    as <- mapM (async . primesThread ref) [x..y]
+primesConcurrent :: Integer -> IO ()
+primesConcurrent  y = do
+    ref <- newIORef []
+    as <- mapM (async . primesThread ref) [2..y]
     mapM_ wait as
     n <- readIORef ref
     print n
 
 -- TODO - Calc primes
 -- TODO - Do what exercise actually wants from us
-primesThread :: IORef Integer -> Integer -> IO()
-primesThread ref n = atomicModifyIORef ref (\acc -> (acc * n, ())) -- Currently calcs fibonacci
+primesThread :: IORef [Integer] -> Integer -> IO()
+primesThread ref n = atomicModifyIORef ref (\acc -> (if is_prime acc n then acc ++ [n] else acc ,())) -- Currently calcs fibonacci
+
+is_prime :: [Integer] -> Integer -> Bool
+is_prime  [] _  = True
+is_prime (x:xs) n = mod n x /= 0 && is_prime xs n 
+
